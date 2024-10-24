cf39ace4fbc86726d06df05ed40f7e40936e85e3
sachin29aug <sachin.sachdeva@gmail.com>
2024-10-24 20:17:54 +0530

    Home page in progress

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -66,7 +66,7 @@ public class HomeController extends Controller {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=5";
+        return url + "?v=6";
     }
 
 }
