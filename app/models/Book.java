cc169035ccf97951c2461a54279e995002b62987
sachin29aug <sachin.sachdeva@gmail.com>
2024-11-02 17:16:45 +0530

    Home page button actions in progress

--- app/models/Book.java
+++ app/models/Book.java
@@ -56,7 +56,6 @@ public class Book extends BaseModel {
         try {
             for(String subCategory : subCategories) {
                 randomBook = getRandomBookBySubcategory(subCategory);
-                ComputerRepository.update(randomBook.id, randomBook.coverImageUrl, randomBook.isbn, randomBook.description, randomBook.previewUrl, randomBook.authorDescription);
                 //randomBook.update();
                 randomBooks.add(randomBook);
             }
@@ -78,11 +77,13 @@ public class Book extends BaseModel {
         //Book randomBook = Book.findByOrderIndex(subCategory, randomIndex);
         Book randomBook = DB.find(Book.class).where().eq("subCategory", subCategory).gt("averageRating", 3.7).gt("ratingCount", 10000).setMaxRows(1).orderBy("RANDOM()").findOne();
         Map<String, String> map = GoogleBookClient.getCoverImageUrlAndIsbn(randomBook.title);
-        randomBook.coverImageUrl = map.get("coverImageUrl");
-        randomBook.isbn = map.get("isbn");
-        randomBook.description = map.get("description");
-        randomBook.previewUrl = map.get("previewUrl");
-        randomBook.authorDescription = map.get("authorDescription");
+        randomBook.setCoverImageUrl(map.get("coverImageUrl"));
+        randomBook.setIsbn(map.get("isbn"));
+        randomBook.setDescription(map.get("description"));
+        randomBook.setPreviewUrl(map.get("previewUrl"));
+        randomBook.setAuthorDescription(map.get("authorDescription"));
+        randomBook.update();
+        //ComputerRepository.update(randomBook.id, randomBook.coverImageUrl, randomBook.isbn, randomBook.description, randomBook.previewUrl, randomBook.authorDescription);
         return randomBook;
     }
 
@@ -146,5 +147,120 @@ public class Book extends BaseModel {
 
     public String subCategory() {
         return this.subCategory;
+    }
+
+    // Getter and Setters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Float getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Float averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    public Integer getRatingCount() {
+        return ratingCount;
+    }
+
+    public void setRatingCount(Integer ratingCount) {
+        this.ratingCount = ratingCount;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getGoodReadsUrl() {
+        return goodReadsUrl;
+    }
+
+    public void setGoodReadsUrl(String goodReadsUrl) {
+        this.goodReadsUrl = goodReadsUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getCoverImageUrl() {
+        return coverImageUrl;
+    }
+
+    public void setCoverImageUrl(String coverImageUrl) {
+        this.coverImageUrl = coverImageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPreviewUrl() {
+        return previewUrl;
+    }
+
+    public void setPreviewUrl(String previewUrl) {
+        this.previewUrl = previewUrl;
+    }
+
+    public String getAuthorDescription() {
+        return authorDescription;
+    }
+
+    public void setAuthorDescription(String authorDescription) {
+        this.authorDescription = authorDescription;
+    }
+
+    public Long getOrderIndex() {
+        return orderIndex;
+    }
+
+    public void setOrderIndex(Long orderIndex) {
+        this.orderIndex = orderIndex;
     }
 }
---
+++ public/javascripts/util.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(document.body).on("click", ".cls-share-btn", function(e) {
+        e.preventDefault();
+        let shareTitle = $(this).data("share-title");
+        let shareUrl = $(this).data("share-url");
+        if (navigator.share) {
+            navigator.share({
+                title: shareTitle,
+                text: "RaLearn helps you discover books that you might have not have discovered yourself. Let's learn: ",
+                url: shareUrl,
+            })
+        }
+    });
+});
