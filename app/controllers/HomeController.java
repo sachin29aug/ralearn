41e4264273750165b4850e84a3697d20b3361326
sachin29aug <sachin.sachdeva@gmail.com>
2024-11-06 19:26:05 +0530

    Feedback page post

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -66,7 +66,7 @@ public class HomeController extends Controller {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=16";
+        return url + "?v=17";
     }
 
 }
