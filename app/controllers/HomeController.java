0eb02da461e68f376d984ef1486509786e0716fe
sachin29aug <sachin.sachdeva@gmail.com>
2024-11-08 11:34:54 +0530

    Book details page - added footer and more options button

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -66,7 +66,7 @@ public class HomeController extends Controller {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=19";
+        return url + "?v=20";
     }
 
 }
