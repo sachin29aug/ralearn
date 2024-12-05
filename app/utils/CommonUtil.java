6c3aa990f27c8dcf8174f05918ab4673c37a64fe
sachin29aug <sachin.sachdeva@gmail.com>
2024-12-05 21:19:40 +0530

    Discover page

--- app/utils/CommonUtil.java
+++ app/utils/CommonUtil.java
@@ -14,7 +14,7 @@ public class CommonUtil {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=32";
+        return url + "?v=33";
     }
 
     // Request related
