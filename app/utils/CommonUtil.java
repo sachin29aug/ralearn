d205fee4e7536927415de7263dbc87a02abc2d4a
sachin29aug <sachin.sachdeva@gmail.com>
2024-12-26 22:07:42 +0530

    Signup flow and code improvements/refactoring. Fixed GB blank covers issue.

--- app/utils/CommonUtil.java
+++ app/utils/CommonUtil.java
@@ -20,7 +20,7 @@ public class CommonUtil {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=38";
+        return url + "?v=39";
     }
 
     // Request related
