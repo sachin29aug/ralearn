4347b1c293f4333875b114bc36afb0821374f71f
sachin29aug <sachin.sachdeva@gmail.com>
2025-04-15 18:10:43 +0530

    UI changes - look and feel

--- app/utils/CommonUtil.java
+++ app/utils/CommonUtil.java
@@ -20,7 +20,7 @@ public class CommonUtil {
     }
 
     public static String getVersionedUrl(String url) {
-        return url + "?v=45";
+        return url + "?v=46";
     }
 
     // Request related
